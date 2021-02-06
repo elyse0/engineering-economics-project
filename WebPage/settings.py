d258768374d362e3b7a88f21b4f354da4e942edc
kolverar <kevincristia@outlook.com>
2021-02-05 19:25:45 -0600

    Allow any host

--- WebPage/settings.py
+++ WebPage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4f6t2_@au)%^d17xf7sob6d8r94dz3ew(&^=3n9jzx9i+e2t4m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
